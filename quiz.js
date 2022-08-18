82eb3617e4e0e322d3dcca269e5f14d6ee18c1e1
Jeune Milford <milford.j@maristcollege.school.nz>
2022-08-18 13:17:33 +1200

    changing code

--- quiz.js
+++ quiz.js
@@ -1,108 +1,92 @@
-var myQuestions = [
-    {
-      question: "What is 10/2?",
-      answers: {
-        a: '3',
-        b: '5',
-        c: '115'
-      },
-      correctAnswer: 'b'
-    },
-    {
-      question: "What is 30/3?",
-      answers: {
-        a: '3',
-        b: '5',
-        c: '10'
-      },
-      correctAnswer: 'c'
+//Function to count correct answers and print results to document 
+function myFunction() {
+
+    //Create variable to count correct answers 
+    var correctAnswers = 0;
+
+    //Create variables for each question's input value
+    var q1 = document.getElementById("q1").value;
+    var q2 = document.getElementById("q2c").checked;
+    var q3 = document.getElementById("q3").value;
+    var q4 = document.getElementById("q4a").checked;
+    var q5 = document.getElementById("q5").value;
+    var q6 = document.getElementById("q6").value;
+    var q7 = document.getElementById("q7c").checked;
+    var q8 = document.getElementById("q8").value;
+    var q9 = document.getElementById("q9").value;
+    var q10 = document.getElementById("q10d").checked;
+    var q11 = document.getElementById("q11b").checked;
+    var q12 = document.getElementById("q12").value;
+    var q13 = document.getElementById("q13b").checked;
+    var q14 = document.getElementById("q14").value;
+    var q15 = document.getElementById("q15a").checked;
+    var q16 = document.getElementById("q16d").checked;
+    var q17 = document.getElementById("q17").value;
+    var q18 = document.getElementById("q18c").checked;
+    var q19 = document.getElementById("q19").value;
+    var q20 = document.getElementById("q20").value;
+
+    //Assign correct answers to field inputs
+      if (q1.toUpperCase() === "CHINA") {
+        correctAnswers += 1;
+    } if (q2) {
+        correctAnswers += 1;
+    } if ((q3.toUpperCase() === "VICTORIA") || (q3.toUpperCase() === "LAKE VICTORIA")) {
+        correctAnswers += 1;
+    } if (q4) {
+        correctAnswers += 1;
+    } if (q5.toUpperCase() === "FLORIDA") {
+        correctAnswers += 1;
+    } if (q6.toUpperCase() === "TANGENT") {
+        correctAnswers += 1;
+    } if (q7) {
+        correctAnswers += 1;
+    } if ((q8.toUpperCase() === "FIVE") || (q8 === "5"))  {
+        correctAnswers += 1;
+    }  if ((q9.toUpperCase() === "NINETY") || (q9 === "90")) {
+        correctAnswers += 1;
+    }  if (q10) {
+        correctAnswers += 1;
+    } if (q11) {
+        correctAnswers += 1;
+    } if (q12.toUpperCase() === "SALT") {
+        correctAnswers += 1;
+    } if (q13) {
+        correctAnswers += 1;
+    }  if (q14.toUpperCase() === "ALUMINIUM") {
+        correctAnswers += 1;
+    }  if (q15) {
+        correctAnswers += 1;
+    }  if (q16) {
+        correctAnswers += 1;
+    }  if (q17.toUpperCase() === "AUSTRIA") {
+        correctAnswers += 1;
+    }  if (q18) {
+        correctAnswers += 1;
+    }  if (q19.toUpperCase() === "FRANCE") {
+        correctAnswers += 1;
+    }  if ((q20.toUpperCase() === "TEN") || (q20 === "10")) {
+        correctAnswers += 1;
     }
-  ];
-  
-  var quizContainer = document.getElementById('quiz');
-  var resultsContainer = document.getElementById('results');
-  var submitButton = document.getElementById('submit');
-  
-  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-  
-  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-  
-    function showQuestions(questions, quizContainer){
-      // we'll need a place to store the output and the answer choices
-      var output = [];
-      var answers;
-  
-      // for each question...
-      for(var i=0; i<questions.length; i++){
-        
-        // first reset the list of answers
-        answers = [];
-  
-        // for each available answer...
-        for(letter in questions[i].answers){
-  
-          // ...add an html radio button
-          answers.push(
-            '<label>'
-              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-              + letter + ': '
-              + questions[i].answers[letter]
-            + '</label>'
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          '<div class="question">' + questions[i].question + '</div>'
-          + '<div class="answers">' + answers.join('') + '</div>'
-        );
-      }
-  
-      // finally combine our output list into one string of html and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-  
-    function showResults(questions, quizContainer, resultsContainer){
-      
-      // gather answer containers from our quiz
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-      
-      // keep track of user's answers
-      var userAnswer = '';
-      var numCorrect = 0;
-      
-      // for each question...
-      for(var i=0; i<questions.length; i++){
-  
-        // find selected answer
-        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-        
-        // if answer is correct
-        if(userAnswer===questions[i].correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-          
-          // color the answers green
-          answerContainers[i].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[i].style.color = 'red';
-        }
-      }
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-  
-    // show questions right away
-    showQuestions(questions, quizContainer);
-    
-    // on submit, show results
-    submitButton.onclick = function(){
-      showResults(questions, quizContainer, resultsContainer);
-    }
-  
-  }
+
+         //Print results (and statement) to document, dependant on outcome
+         if (correctAnswers === 20) {
+        document.getElementById("result").innerHTML = "Congratulations! You got all 20 answers correct";}
+    else if (correctAnswers >= 15) {
+        document.getElementById("result").innerHTML = "Congratulations! You got " + correctAnswers + " answers correct";}
+    else if (correctAnswers >= 10) {
+        document.getElementById("result").innerHTML = "You got " + correctAnswers + " answers correct";}
+    else if (correctAnswers >= 1) {
+        document.getElementById("result").innerHTML = "Too Bad! You only got " + correctAnswers + " answers correct";}
+    else if (correctAnswers === 1) {
+        document.getElementById("result").innerHTML = "Too Bad! You only got 1 answer correct";}
+    else if (correctAnswers === 0) {
+        document.getElementById("result").innerHTML = "You didn't answer a single answer correctly!";}
+
+        //Print correct answers to form
+        document.getElementById("answers").innerHTML = "Correct answers: (1)China (2)Chile (3)Victoria (4)Canberra (5)Florida (6)Tangent (7)Triskaidecagon (8)5 (9)90 (10)150 (11)Bromine (12)Salt (13)deliquescence (14)Aluminium (15)Hydrogen (16)1914 (17)Austria (18)Dallas (19)France (20)10";
+
+        //Reset contents of form
+        document.getElementById("quiz-form").reset();
+}
+
